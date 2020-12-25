90d658ce024bb02d4872f5be5b0b8202e95a424f
luoxian <wtt19940512577>
2020-12-25 20:18:34 +0800

    图片和页面

---
+++ src/main/java/com/business/controller/BaseUrlController.java
@@ -0,0 +1,31 @@
+package com.business.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 凤凰小哥哥
+ * @date 2020-12-25
+ */
+@Controller
+@RequestMapping("/base")
+public class BaseUrlController {
+
+    @Value("${spring.application.baseUrl}")
+    private String baseUrl;
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("baseUrl",baseUrl);
+        return "/shoplogin";
+    }
+
+    @GetMapping("/footer")
+    public String footer(Model model){
+        model.addAttribute("baseUrl",baseUrl);
+        return "/footer";
+    }
+}
---
+++ src/main/java/com/business/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.business.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 凤凰小哥哥
+ * @date 2020-12-25
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+}
